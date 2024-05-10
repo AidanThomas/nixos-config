ef7ba7eeecab250fbf5320e25198f8036bf4c913
Aidan Thomas <aidant@agylia.com>
2024-05-10 21:37:49 +0100

    Add brightness widget

--- wm/statusbars/eww.nix
+++ wm/statusbars/eww.nix
@@ -5,4 +5,16 @@
         package = pkgs.eww-wayland;
         configDir = ./eww;
     };
+
+    home.packages = [
+        pkgs.networkmanager_dmenu
+        pkgs.brightnessctl
+    ];
+
+    home.file = {
+        "/home/aidant/.config/networkmanager-dmenu/config.ini".text = ''
+        [dmenu]
+        dmenu_command = wofi --dmenu
+        '';
+    };
 }
