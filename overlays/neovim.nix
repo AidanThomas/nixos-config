2d8fdc0e429d5f747b3661fcbf329709e9ba0e00
Aidan Thomas <aidant@agylia.com>
2025-05-25 22:55:24 +0100

    chore: bump neovim version

--- overlays/neovim.nix
+++ overlays/neovim.nix
@@ -1,15 +1,9 @@
 self: super: {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
-    version = "v0.11.0";
+    version = "v0.11.1";
     src = super.fetchurl {
       url = "https://github.com/neovim/neovim/archive/${version}.tar.gz";
-      hash = "sha256-aCbEgS6WmV0pqYWG1E++58myBFSF1Q0XS+zW1SQrMxk=";
+      hash = "sha256-/+f5p2M+2JX/atsQOa91Fs1kU3FciImthEtvo5w99EM=";
     };
-    # src = super.fetchFromGitHub {
-    #   owner = "neovim";
-    #   repo = "neovim";
-    #   rev = "release-0.11";
-    #   sha256 = "3684KPUTq7ZBluJvvUnywIgDcUwJ6NAtlFxh5pkE0Cg=";
-    # };
   });
 }
