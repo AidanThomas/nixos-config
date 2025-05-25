a79835cb43c54a0ade42978d858f6c43b17a4fbb
Aidan Thomas <aidant@agylia.com>
2025-05-25 22:54:29 +0100

    feat(eww): cleanup config derivation

--- components/wm/statusbars/eww.nix
+++ components/wm/statusbars/eww.nix
@@ -2,12 +2,17 @@
   pkgs,
   settings,
   ...
-}: {
+}: let
+  ewwConfig = fetchGit {
+    url = "https://github.com/AidanThomas/eww-config";
+    rev = "629cfba1ad50bf53e72c64a4b8f4eaa87077c64c";
+  };
+in {
   programs.eww = {
     enable = true;
     configDir = pkgs.symlinkJoin {
       name = "eww";
-      paths = [./eww ../scripts/eww/${settings.usr.display.wm}];
+      paths = [ewwConfig "${ewwConfig}/${settings.usr.display.wm}"];
     };
   };
 
