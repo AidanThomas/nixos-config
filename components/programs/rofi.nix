d87cc0e73f1bca2954c1f6c41a5b8d99a89649bd
Aidan Thomas <aidant@agylia.com>
2025-05-26 00:38:44 +0100

    feat(rofi): change keybinds

--- components/programs/rofi.nix
+++ components/programs/rofi.nix
@@ -17,6 +17,11 @@
       display-Network = " 󰤨  Network ";
       display-run = "   Run ";
       sidebar-mode = true;
+      kb-mode-next = "Control+l";
+      kb-mode-previous = "Control+h";
+      kb-mode-complete = "Control+Shift+l";
+      kb-remove-to-eol = "";
+      kb-remove-char-back = "BackSpace,Shift+BackSpace";
     };
   };
 }
